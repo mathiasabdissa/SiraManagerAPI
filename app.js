a4b57db7ada3d84588ea82218227ff02f210f0ed
1Mathias <mathiasabdisa@gmail.com>
2022-01-26 02:35:54 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { mongoose } = require('./db/mongoose')
+
+const host = 'localhost'
+const port = 8000
+// Load in the mongoose models
+const { List, Task } = require('./db/models');
+
+//load Middleware
+app.use(bodyParser.json());
+
+/* Route handlers */
+
+/* LIST ROUTES */
+
+/**
+ * GET /lists
+ * Purpose: Get all lists
+ */
+app.get('/lists', (req, res) => {
+    // We want to return an array of all the lists that belong to the authenticated user 
+    List.find({}).then((lists) => {
+        res.send(lists);
+    });
+})
+
+/**
+ * POST /lists
+ * Purpose: Create a list
+ */
+app.post('/lists', (req, res) => {
+    // We want to create a new list and return the new list document back to the user (which includes the id)
+    // The list information (fields) will be passed in via the JSON request body
+    let title = req.body.title;
+    let newlist = new List({
+        title
+    });
+    newlist.save().then((listDoc) => {
+        //the full list document is returned
+        res.send(listDoc);
+    })
+})
+
+/**
+ * PATCH /lists/:id
+ * Purpose: Update a specified list
+ */
+
+app.patch('/lists/:id', (req, res) => {
+    // We want to update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+        //res.send({ 'message': 'updated successfully'});
+    });
+
+})
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete a list
+ */
+app.delete('/lists/:id', (req, res) => {
+    // We want to delete the specified list (document with id in the URL)
+    List.findOneAndRemove({
+        _id: req.params.id
+    }).then((removedListDoc) => {
+        res.send(removedListDoc);
+    })
+});
+
+
+/**
+ * GET /lists/:listId/tasks
+ * Purpose: Get all tasks in a specific list
+ */
+app.get('/lists/:listId/tasks', (req, res) => {
+    // We want to return all tasks that belong to a specific list (specified by listId)
+    Task.find({
+        _listId: req.params.listId
+    }).then((tasks) => {
+        res.send(tasks);
+    })
+});
+
+
+/**
+ * GET /lists/:listId/tasks
+ * Purpose: Get specific task in a specific list
+ */
+ app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    // We want to return all tasks that belong to a specific list (specified by listId)
+    Task.find({
+        _id:req.params.taskId,
+        _listId: req.params.listId
+    }).then((task) => {
+        res.send(task);
+    })
+});
+/**
+ * POST /lists/:listId/tasks
+ * Purpose: Create a new task in a specific list
+ */
+app.post('/lists/:listId/tasks', (req, res) => {
+    // We want to create a new task in a list specified by listId
+
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+    newTask.save().then((newTaskDoc) => {
+        res.send(newTaskDoc);
+    })
+})
+
+
+/**
+ * PATCH /lists/:listId/tasks/:taskId
+ * Purpose: Update an existing task
+ */
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    // We want to update an existing task (specified by taskId)
+
+    Task.findOneAndUpdate({
+        _id: req.params.taskId, 
+        _listId: req.params.listId
+    }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+    })
+});
+/**
+ * DELETE /lists/:listId/tasks/:taskId
+ * Purpose: Delete a task
+ */
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndRemove({
+        _id:req.params.taskId,
+        _listId:req.params.listId
+    }).then((removedTaskDoc)=>{
+        res.send(removedTaskDoc);
+    })
+}); 
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening on port 3000");
+})
---
+++ db/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TaskManager', {
+    useCreatendex: true, 
+    useFindAndModify: false, 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => {
+    console.log("Connected to MongoDB successfully :)");
+}).catch((e) => {
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
+
+// To prevent deprectation warnings (from MongoDB native driver)
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+
+module.exports = {
+    mongoose
+};
